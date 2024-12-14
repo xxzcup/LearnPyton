4b1846871a2538ef791c986637a90f96e2a1d033
xxzcup <ilhamabdul116@hotmail.com>
2024-12-14 07:44:19 +0700

    Add files via upload

---
+++ Perulangan.py
@@ -0,0 +1,12 @@
+"""
+TODO:
+Buatlah sebuah variabel bertipe list bernama "evenNumber" dengan ketentuan:
+- variabel tersebut menampung bilangan genap dari 0 hingga 500 (ingat 0 dan 500 termasuk).
+
+Tips:
+Anda bisa menggunakan loop dan if atau list comprehension untuk memudahkan.
+"""
+
+# TODO: Silakan buat kode Anda di bawah ini.
+evenNumber = list (range(0,501,2))
+print(evenNumber)
